50e3a5cb57a9ea47b42ebbd676f9924fa2296b08
Moritz Rumpf <mrumpf@ebay.com>
2018-11-08 13:43:55 +0100

    Add sass

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,3 +1,4 @@
 import { SomeEnum } from "../src/types";
+import "./about.scss"
 
-export default () => <p>There is an enum: {SomeEnum.someValue.toString()}</p>
+export default () => <p className="scssTest">There is an enum: {SomeEnum.someValue.toString()}</p>
