95761495af61837ad30944a74fbf3adc79570f84
Sonny Piers <sonny@fastmail.net>
2018-11-16 18:44:03 +0100

    Remove @zeit/next-css as it is not necessary to reproduce the bug

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 // next.config.js
 const withTypescript = require('@zeit/next-typescript')
 const withSass = require('@zeit/next-sass')
-const withCSS = require('@zeit/next-css')
 
-module.exports = withCSS(withSass(withTypescript()))
+module.exports = withSass(withTypescript())
